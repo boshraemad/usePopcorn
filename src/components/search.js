b7fac114545e7755c00f4e6eedc5de308116eef3
boshraemad <boshraemad2004@icloud.com>
2025-07-20 02:04:57 +0300

    useRef to focus on search input element

--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,25 @@
 import React from 'react'
-
-
+ import {useEffect , useRef} from "react";
 export default function Search({query , setQuery}) {
+  //set to null when select Dom Element
+  const  inputEl=useRef(null);
+
+   //focus on element
+  useEffect(()=>{
+    inputEl.current.focus();
+  },[])
+
+  //listen to "Enter" keyPress
+  useEffect(()=>{
+    if(document.activeElement === inputEl) return;
+    const callback=(e)=>{
+      if(e.code === "Enter"){
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown",callback);
+  },[setQuery])
 
   return (
     <input
@@ -10,6 +28,7 @@ export default function Search({query , setQuery}) {
     placeholder="Search movies..."
     value={query}
     onChange={(e) => setQuery(e.target.value)}
+    ref={inputEl}
   />
   )
 }
